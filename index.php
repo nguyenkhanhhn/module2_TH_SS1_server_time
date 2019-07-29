0c2a9d8086f4ffd97d7ff77fa87106f738905c91
nguyen_khanh <nguyenkhanheng3794@gmail.com>
2019-07-29 21:54:32 +0700

    bt

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo"Xin chao";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Thời gian hiện tại</title>
+</head>
+<body>
+<h1> Bây giờ là :<?php echo  date('Y-M-d h:m:s') ?> </h1>
+</body>
+</html>
